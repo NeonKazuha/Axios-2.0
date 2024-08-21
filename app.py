b8e42694f341e38cc88eefe4bb19a2612398bce6
NeonKazuha <tanudeshtd2005@gmail.com>
2024-08-22 02:32:27 +0530

    Storing previous generations.

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ model = load_model()
 
 st.title('Shakespearean Text Generator')
 
+if 'generated_text_history' not in st.session_state:
+    st.session_state.generated_text_history = []
+
 input_text = st.text_area('Enter some starting text:', 'In times long past')
 max_new_tokens = st.slider('Number of tokens to generate:', 1, 10000, 100)
 
@@ -25,9 +28,18 @@ if st.button('Generate Text'):
         generated_tokens = model.generate(context, max_new_tokens=max_new_tokens)
     
     generated_text = decode(generated_tokens[0].tolist())
+    st.session_state.generated_text_history.append(generated_text)
     
     st.write('Generated Text:')
     st.write(generated_text)
+
+if st.button('Previous'):
+    if st.session_state.generated_text_history:
+        st.session_state.generated_text_history.pop()
+        if st.session_state.generated_text_history:
+            generated_text = st.session_state.generated_text_history[-1]
+            st.write('Generated Text:')
+            st.write(generated_text)
 
 st.sidebar.header('Model Information')
 st.sidebar.write(f'Number of parameters: {sum(p.numel() for p in model.parameters())}')
